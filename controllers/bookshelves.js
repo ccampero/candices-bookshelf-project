9db704deffbb74809d46a0cb312a36ca4d672f4a
mccampero <mccampero2@gmail.com>
2024-07-10 12:19:49 -0400

    build the navbar

--- controllers/bookshelves.js
+++ controllers/bookshelves.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const bookshelvesController = require('../controllers/bookshelves.js');
+
+
+
+
+
+router.get('/', (req, res) => {
+    res.send('hello bookshelves index route');
+});
+
+module.exports = router;
 
 const User = require('../models/user.js');
 
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 const authController = require('./controllers/auth.js');
+const bookshelvesController = require('./controllers/bookshelves.js')
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
