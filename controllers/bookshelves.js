cd859ba01590da5e01d5a2d8a9b08630527c80b6
mccampero <mccampero2@gmail.com>
2024-07-11 13:17:54 -0400

    build the show page

--- controllers/bookshelves.js
+++ controllers/bookshelves.js
@@ -36,4 +36,18 @@ router.get('/new', async (req, res) => {
     }
 });
 
+router.get('/:applicationId', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      const bookshelf = currentUser.bookshelves.id(req.params.bookshelfId);
+      res.render('bookshelves/show.ejs', {
+        bookshelf: bookshelf,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+
+
 module.exports = router;
